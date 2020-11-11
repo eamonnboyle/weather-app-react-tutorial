548e7df089b0ca94b33991b46453feae13be09db
Eamonn Boyle <eamonn.boyle@instil.co>
2020-11-11 17:04:58 +0000

    Add Step 2

---
+++ step-2/src/App.tsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [locationSearch, setLocationSearch] = useState('Paris');
+  const [locations, setLocations] = useState(['Belfast', 'Dublin']);
+
+  const disableSearch = locationSearch.trim() === '';
+  const addLocation = () => {
+    setLocations([locationSearch, ...locations]);
+    setLocationSearch('');
+  };
+
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <div>
+        <label>
+          Add Location
+          <input className="ml-1 mr-1" type="text" value={locationSearch}
+                 onChange={e => setLocationSearch(e.target.value)}/>
+        </label>
+        <button className="btn btn-primary"
+                onClick={addLocation} disabled={disableSearch}>Search</button>
+      </div>
+
+      <div>
+        <h2>Locations</h2>
+        <table className="table table-hover">
+          <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+          </thead>
+          <tbody>
+          {locations.map(location =>
+            <tr><td>{location}</td></tr>
+          )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
